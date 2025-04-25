cc61dd7ddcc6280245859e87aae740dfbd2fcb34
SOWPARNIKA M <sowparnikasatheesh248@gmail.com>
2025-04-26 02:29:32 +0530

    api

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Products.css';
 
-const API = import.meta.env.VITE_API_URL;
+const API = "https://ecomm-site-server.onrender.com";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
